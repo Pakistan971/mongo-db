6ec9f02d6aface9f0d05ab5fa1c158f1006a1ba7
Shahbaz Ali <161756431+Pakistan971@users.noreply.github.com>
2024-10-19 18:14:29 +0000

    context updated

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 from setuptools import setup, find_packages
 from typing import List
 
+HYPEN_E_DOT = '-e .'
+
+def get_requirement(file_path: str) -> List[str]:
+    requirements = []
+    try:
+        with open(file_path) as f:
+            requirements = f.readlines()
+            requirements = [req.replace("\n", "") for req in requirements]
+            if HYPEN_E_DOT in requirements:
+                requirements.remove(HYPEN_E_DOT)
+    except FileNotFoundError:
+        print(f"File {file_path} not found. Please check the path.")
+    return requirements
+
+
 with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     
-   
+    long_description = f.read()
+
 
 __version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
+REPO_NAME = "mongo-db"
+PKG_NAME = "MongoConnect"
+AUTHOR_USER_NAME = "Pakistan971"
 AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
 
 setup(
@@ -18,13 +33,12 @@ setup(
     author_email=AUTHOR_EMAIL,
     description="A python package for connecting with database.",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",  # Corrected here
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    )
-
-
+    install_requires=get_requirement("requirements_dev.txt")  # Fixed path
+)
