5493a701236f0deb6f7e212a2caa448557313507
Shahbaz Ali <161756431+Pakistan971@users.noreply.github.com>
2024-10-19 11:24:51 +0000

    code commit

--- template.py
+++ template.py
@@ -1,50 +1,34 @@
 import os
 from pathlib import Path
-import logging
 
-logging.basicConfig(level=logging.INFO)
+package_name = "mongodb_connect"
 
-#listing all the required files and folders
-list_of_files =  [
-    ".github/workflows/.gitkeep",
-    "src/__init__.py",
-
-    "src/components/__init__.py",
-    "src/components/data_ingestion.py",
-    "src/components/data_transformation.py",
-    "src/components/model_trainer.py",
-    "src/components/moel_evaluation.py",
-
-    "src/logger/logging.py",
-    "src/exception/exception.py",
-
-    "src/pipeline/__init__.py",
-    "src/pipeline/training_pipeline.py",
-    "src/pipeline/prediction_pipeline.py",
-
-    "src/utils/__init__.py",
-    "src/utils/utils.py",
-    "test/unit/__init__.py",
-    "test/integration/__init__.py",
-    "init_setup.sh",
-    "requirements.txt",
-    "requirements_dev.txt",
-    "setup.py",
-    "setup.cfg",
-    "pyproject.toml",
-    "tox.ini",
-    "experiment/experiments.ipynb"
+list_of_files = [
+   ".github/workflows/ci.yaml",
+   "src/__init__.py",
+   f"src/{package_name}/__init__.py", 
+   f"src/{package_name}/mongo_crud.py", 
+   "tests/__init__.py",
+   "tests/unit/__init__.py",
+   "tests/unit/unit.py",
+   "tests/integration/__init__.py",
+   "tests/integration/int.py",
+   "init_setup.sh",
+   "requirements.txt", 
+   "requirements_dev.txt",
+   "setup.py",
+   "setup.cfg",
+   "pyproject.toml",
+   "tox.ini",
+   "experiments/experiments.ipynb", 
 ]
 
-#creating the files and folders
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir,filename = os.path.split(filepath)
+    filedir, filename = os.path.split(filepath)
     if filedir != "":
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for file: {filename}")
+        os.makedirs(filedir, exist_ok=True)
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
-        with open(filepath,"w") as f:
-            pass
-
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass # create an empty file
